1f25d3ee17cdec54bb975106d88cfb6cbf5c4c50
Angela Gola <angelagola.ko@gmail.com>
2022-10-11 11:49:57 -0400

    init

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,21 @@
+const postsResolvers = require('./posts');
+const usersResolvers = require('./users');
+const commentsResolvers = require('./comments');
+
+module.exports = {
+    Post: {
+        likeCount: (parent) => parent.likes.length,
+        // SAME AS...likeCount(parent){
+        //     console.log(parent);
+        //     return parent.likes.length;
+        commentCount: (parent) => parent.comments.length
+    },
+    Query: {
+        ...postsResolvers.Query
+    },
+    Mutation: {
+        ...usersResolvers.Mutation,
+        ...postsResolvers.Mutation,
+        ...commentsResolvers.Mutation
+    }
+};
